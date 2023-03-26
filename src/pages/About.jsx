85efae276983bcf059350584a0be84dcedd6240a
Mustafa <57066099+mohdmustafa0111@users.noreply.github.com>
2023-03-26 21:13:42 +0530

    removed unnecessary piece of code

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,17 +1,12 @@
 import HeroSection from "../components/HeroSection";
-import { useContext } from "react";
-import AppContext from "../context/ProductContext";
 
 const About = () => {
-  const { myName } = useContext(AppContext);
-
   const data = {
     name: "Angel Ecommerce",
   };
 
   return (
     <>
-      {myName}
       <HeroSection myData={data} />
     </>
   );
